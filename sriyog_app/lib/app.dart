34e9b4d2d25ba2005fb3bcc1f95b06c0114bc047
rn-byte <ramthakur977+github@gmail.com>
2024-09-17 23:06:35 +0545

    This is 1st commit

---
+++ sriyog_app/lib/app.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Sriyog App',
+      home: Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(
+            color: Colors.blue,
+          ),
+        ),
+      ),
+    );
+  }
+}
