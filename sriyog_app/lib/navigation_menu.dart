3e340c8d777a5efa4ab0f15723492c665c718aae
rn-byte <ramthakur977+github@gmail.com>
2024-09-18 16:34:20 +0545

    theme added in Navigation Bar

--- sriyog_app/lib/navigation_menu.dart
+++ sriyog_app/lib/navigation_menu.dart
@@ -1,16 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:sriyog_app/utils/helpers/helper.dart';
 
 class NavigationMenu extends StatelessWidget {
   const NavigationMenu({super.key});
 
   @override
   Widget build(BuildContext context) {
+    final isDark = SpHelper.isDarkMode(context);
     return Scaffold(
       bottomNavigationBar: NavigationBar(
           height: 60,
           elevation: 0,
-          backgroundColor: Colors.white,
-          indicatorColor: Colors.grey[300],
+          backgroundColor: isDark ? Colors.black : Colors.white,
+          indicatorColor: isDark ? Colors.grey.withOpacity(0.6) : Colors.grey[300],
           destinations: const [
             NavigationDestination(icon: Icon(Icons.home_outlined), label: 'Home'),
             NavigationDestination(icon: Icon(Icons.work_outline), label: 'Professions'),
