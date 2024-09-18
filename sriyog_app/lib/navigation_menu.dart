cf997963668fbb3cbde679c2a536b54f22a5114d
rn-byte <ramthakur977+github@gmail.com>
2024-09-18 15:13:05 +0545

    poppins font, navigationMenu & custom textTheme added

---
+++ sriyog_app/lib/navigation_menu.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class NavigationMenu extends StatelessWidget {
+  const NavigationMenu({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
